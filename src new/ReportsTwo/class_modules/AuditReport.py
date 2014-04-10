ce3da968d276b283928268b9d375bfd9d0ddaaaa
JohanEsterhuyse <dj.esterhuyse@ymail.com>
2014-04-11 00:43:18 +0200

    Audit log and audit.csv uploaded

--- src new/ReportsTwo/class_modules/AuditReport.py
+++ src new/ReportsTwo/class_modules/AuditReport.py
@@ -1,13 +1,28 @@
+import csv
 from class_modules.ReportRequest import ReportRequest
 from class_modules.ReportGenerator import Report
 
 class AuditReport(Report):
-  __init__(self, doc_heading, column_headings, body):
-    createReport(
-	self.doc_heading = doc_heading
-    self.column_headings = column_headings
-    self.body = body
+  __init__(self,startDate,endDate,course):
+    createReport(startDate,endDate,course)
+	
     
-  def createReport(self,):
-    #getAudit
+  def createReport(self,startDate,endDate,course):
+	self.doc_heading = "Audit Report for " + course + "from " + startDate + "to " + endDate
+    #self.column_headings = getAuditHeadings(startDate,endDate,course) 
+	#self.body = getAuditBody(startDate,endDate,course)
+	#csv in use for now
+	with open('audit.csv', 'rb') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=',')
+	
+	for cnt, row in enumerate(csvreader):
+ 			# store the data
+ 			body.append(row)
+
+	def returnHeader(self)
+		return self.doc_heading
+	
+	
+	def returnBody(self)
+		return self.body
 	
