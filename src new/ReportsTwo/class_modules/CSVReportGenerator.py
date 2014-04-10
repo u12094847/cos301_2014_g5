86ee66af95e758b435cdbc6088290f720e9d2b14
HandreWatkins <kuria.okami@gmail.com>
2014-04-11 01:46:12 +0200

    csvgen

--- src new/ReportsTwo/class_modules/CSVReportGenerator.py
+++ src new/ReportsTwo/class_modules/CSVReportGenerator.py
@@ -1,10 +1,21 @@
+import csv
 from class_modules.ReportRequest import ReportRequest
 from class_modules.ReportGenerator import ReportGenerator
 
 class CSVReportGenerator(ReportGenerator):
-  def __init__(self, _request):		#Constructor
+  __init__(self, _request):		#Constructor
     self.reportRequest = _request
   
-  
-  def generateReport(self):		#This method overrides the one in its superclass
+  #acceptes list of lists in data
+  def generateReport(self,course,data):		#This method overrides the one in its superclass
     #implementation goes here
+
+   self.doc_heading = course+".csv"
+
+    fileName = self.doc_heading
+    with open(fileName, 'wb') as csvOutPut:
+        filewriter = csv.writer(csvOutput, quoting=csv.QUOTE_ALL)
+
+        filewriter.writerow(rowdata)
+
+
