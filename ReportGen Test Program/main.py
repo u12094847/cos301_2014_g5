c06c38605c56b399b689dce24764dfb381f0e5c5
Christopher Moodley <kmzls777@gmail.com>
2014-04-10 21:10:57 +0200

    Slight mod

--- ReportGen Test Program/main.py
+++ ReportGen Test Program/main.py
@@ -10,11 +10,11 @@ course_name = 'COS 212'
 def create_report(data_file):
     pdf_name = course_name + ' Marksheet.pdf'
     c = canvas.Canvas(pdf_name, pagesize = A4)
-    heighty = 700
+    heighty = 750
     #header text
     c.drawCentredString(125, 800, course_name)
     testr1 = 'Student Number P1 ST1 T1 T2'
-    c.drawCentredString(125, 700, testr1)
+    c.drawCentredString(125, 750, testr1)
     marks_data = csv.reader(open(data_file, "rb"))
     for row in marks_data:
         st_num = row[0]
@@ -23,7 +23,7 @@ def create_report(data_file):
         t1 = row[3]
         t2 = row[4]
         
-        heighty -= 100
+        heighty -= 20
         testr2 = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[4]
         c.drawCentredString(125, heighty, testr2)
         
