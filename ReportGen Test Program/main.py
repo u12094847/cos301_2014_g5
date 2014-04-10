5e62d7435338f5a67f84b64d04e2958bab47cf10
Christopher Moodley <kmzls777@gmail.com>
2014-04-10 20:59:42 +0200

    Working Code
    Now onto formatting

--- ReportGen Test Program/main.py
+++ ReportGen Test Program/main.py
@@ -5,16 +5,16 @@ import csv
 
 data_file = 'data.csv'
 course_name = 'COS 212'
-height = 700
+
 
 def create_report(data_file):
     pdf_name = course_name + ' Marksheet.pdf'
-    c = canvas.Canvas(pdf_name, pagesize=portrait(A4))
-    
+    c = canvas.Canvas(pdf_name, pagesize = A4)
+    heighty = 700
     #header text
-    c.drawCenteredString(125, 800, course_name)
+    c.drawCentredString(125, 800, course_name)
     testr1 = 'Student Number P1 ST1 T1 T2'
-    c.drawCenteredString(125, 700, testr1)
+    c.drawCentredString(125, 700, testr1)
     marks_data = csv.reader(open(data_file, "rb"))
     for row in marks_data:
         st_num = row[0]
@@ -23,9 +23,10 @@ def create_report(data_file):
         t1 = row[3]
         t2 = row[4]
         
+        heighty -= 100
         testr2 = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[4]
-        c.drawCenteredString(125, height, testr2)
-        height -= 100
+        c.drawCentredString(125, heighty, testr2)
+        
     #save pdf page, use for each page
     c.showPage()
     #print 'created'
