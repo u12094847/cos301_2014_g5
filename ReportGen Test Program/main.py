4b0106b0053d817e4f1d357442996f34751983b4
Christopher Moodley <kmzls777@gmail.com>
2014-04-10 23:02:41 +0200

    Format Clean-up
    The PDF generated has some format to it

--- ReportGen Test Program/main.py
+++ ReportGen Test Program/main.py
@@ -10,23 +10,33 @@ course_name = 'COS 212'
 def create_report(data_file):
     pdf_name = course_name + ' Marksheet.pdf'
     c = canvas.Canvas(pdf_name, pagesize = A4)
-    heighty = 750
+    heighty = 770
+    widthy = 30
     #header text
-    c.drawCentredString(125, 800, course_name)
-    testr1 = 'Student Number P1 ST1 T1 T2'
-    c.drawCentredString(125, 750, testr1)
+    c.drawCentredString(110, 800, course_name + ' Marksheet')
+
     marks_data = csv.reader(open(data_file, "rb"))
     for row in marks_data:
-        st_num = row[0]
-        p1 = row[1]
-        st1 = row[2]
-        t1 = row[3]
-        t2 = row[4]
-        
+        widthy += 70
         heighty -= 20
-        testr2 = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[4]
-        c.drawCentredString(125, heighty, testr2)
         
+        c.drawCentredString(widthy, heighty, row[0])
+        widthy += 70
+        c.drawCentredString(widthy, heighty, row[1])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[2])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[3])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[4])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[5])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[6])
+        widthy += 40
+        c.drawCentredString(widthy, heighty, row[7])
+        
+        widthy = 30
     #save pdf page, use for each page
     c.showPage()
     #print 'created'
