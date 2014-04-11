d3a0f6a9bf564e75279697804e8de57458c7bc3e
legodi-301 <u29302732@tuks.co.za>
2014-04-11 10:49:23 +0200

    Updated Assessment Report

--- src new/ReportsTwo/class_modules/AssessmentReport.py
+++ src new/ReportsTwo/class_modules/AssessmentReport.py
@@ -1,34 +1,35 @@
 import csv
 from class_modules.ReportRequest import ReportRequest
 from class_modules.ReportGenerator import Report
+#from Report import Report
 
 class AssessmentReport(Report):
   def __init__(self,assessment,course):
-		createReport(assessment,course)
+        self.createReport(assessment,course)
   
-  def average(array):
-	    	return sum(array) * 1.0 / len(array)
+  #def average(array):
+        #return sum(array) * 1.0 / len(array)
   
   def createReport(self,assessment,course):
-	self.doc_heading = "Assessment Report for " + course + assessment
+	self.doc_heading = "Assessment Report for " + course +" "+assessment
     #self.column_headings = getAssessment(assessment,course) 
 	#self.body = getAuditBody(assessment,course)
 	#csv in use for now
-	self.total = 0
-	self.count = 0
-	with open('assessment.csv', 'rb') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter=',')
-	for cnt, row in enumerate(csvreader):
+	total = 0
+	#self.count = 0
+        assessments = []
+        csvreader = csv.reader(open('assessment.csv', "rb"))
+	for count, row in enumerate(csvreader):
  		# store the data
- 		body.append(row)
-		if count > 0
-			total += row[1]
-		count += 1
-	ave = average(total)
-	body.append("Average",ave)
-	
+ 		assessments.append(row)
+		#if count > 0 :
+                    #total += row[1]
+		#count += 1
+	#ave = average(total)
+	#self.body.append("Average",ave)
+	self.body = assessments
   def returnHeader(self):
-			return self.doc_heading
+        return self.doc_heading
 
   def returnBody(self):
-			return self.body
+        return self.body
