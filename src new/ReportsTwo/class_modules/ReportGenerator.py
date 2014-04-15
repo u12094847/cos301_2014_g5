59226bc2640df6d075c0de892e7a0c7b75138e27
Mbulungo Justin Musetsho <mbulungom41@gmail.com>
2014-04-15 12:41:15 +0200

    New generateReport functions added

--- src new/ReportsTwo/class_modules/ReportGenerator.py
+++ src new/ReportsTwo/class_modules/ReportGenerator.py
@@ -1,9 +1,17 @@
 from class_modules.ReportRequest import ReportRequest
 
 class ReportGenerator:
-  def __init__(self, _request):		#Constructor
-    self.reportRequest = _request
+  def __init__(self):		#Constructor
+    test = "" #N.B. Remove this line of code when you start
   
   
-  def generateReport(self):		#ReportGenerator abstract method. This method will be implemented by respective subclasses
-    test = "" #N.B. Remove this line of code when you start coding
+ 
+  
+  def generateReport(self, module, assessment, outputType)
+    test = "" #N.B. Remove this line of code when you start
+	
+  def generateReport(module, studentNo, assessments, outputType)
+    test = "" #N.B. Remove this line of code when you start
+	
+  def generateReport(module, userID, alteredTable, dateFrom, dateTo, outputType)
+    test = "" #N.B. Remove this line of code when you start
