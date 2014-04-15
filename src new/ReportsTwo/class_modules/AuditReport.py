53fddca421b817aac64f1c7b740213c7b641d03a
JohanEsterhuyse <dj.esterhuyse@ymail.com>
2014-04-15 04:56:12 +0200

    Added complete AuditReport and 4 Text files that need to be checked and aproved that needs to be given to other groups
    Audit log done.
    API all reports provide, How to call ReportGenerators/use our
    functionality, this needs to go to Data IO.
    Functions from business logic file that we use and also goes to BL.
    Constructors for Reports that contain all the report constructors in one
    text file for convenience.

--- src new/ReportsTwo/class_modules/AuditReport.py
+++ src new/ReportsTwo/class_modules/AuditReport.py
@@ -3,26 +3,18 @@ from class_modules.ReportRequest import ReportRequest
 from class_modules.ReportGenerator import Report
 
 class AuditReport(Report):
-  def __init__(self,startDate,endDate,course):
-    createReport(startDate,endDate,course)
+  def __init__(self,reportName,headings,data):
+    self.reportName = reportName
+	self.headings = headings
+	self.data = data
 	
-    
-  def createReport(self,startDate,endDate,course):
-	self.doc_heading = "Audit Report for " + course + "from " + startDate + "to " + endDate
-    #self.column_headings = getAuditHeadings(startDate,endDate,course) 
-	#self.body = getAuditBody(startDate,endDate,course)
-	#csv in use for now
-	with open('audit.csv', 'rb') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter=',')
-	
-	for cnt, row in enumerate(csvreader):
- 			# store the data
- 			body.append(row)
-
-	def returnHeader(self)
-		return self.doc_heading
+	def getReportName(self)
+		return self.reportName
 	
 	
-	def returnBody(self)
-		return self.body
+	def getData(self)
+		return self.data
+		
+	def getHeadings
+		return self.headings
 	
